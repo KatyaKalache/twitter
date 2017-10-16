56e348bdaed4439bd4bb506b7cc842a878b35824
Katya Kalache <ekaterina@kalache.fr>
2017-10-15 17:13:07 -0700

    Adding new functionality: input opens in a dialog window when tweet button is clicked from the header

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Opens the dialog window when Tweet button is clicked
+// Closes the window when Tweet button is clicked fron the dialog window
+(function() {  
+  var dialog = document.getElementById('window');  
+  document.getElementById('show').onclick = function() {  
+    dialog.show();
+    };
+  document.getElementById('add_text').onclick = function() {  
+    dialog.close();  
+    };
+})();
+
+// Posts the text from input window to the feed
 $("#add_text").click(function () {
   $('#feed').append($('#text_name').val());  
 });
